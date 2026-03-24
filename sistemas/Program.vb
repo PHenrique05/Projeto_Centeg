9fbf4db0da32c959856d6b7311ecc7a5a2c4a124
PHenrique05 <267174002+PHenrique05@users.noreply.github.com>
2026-03-24 20:49:31 -0300

    sistemas

---
+++ sistemas/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim sistema(3) As String
+        sistema(0) = "Linux"
+        sistema(1) = "Windows"
+        sistema(2) = "IOS"
+        Console.WriteLine(sistema(0))
+        Console.WriteLine(sistema(1))
+        Console.WriteLine(sistema(2))
+    End Sub
+End Module
