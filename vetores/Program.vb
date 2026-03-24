9fff839be8fd761988e1a7b4a8695540ef7399f1
weslenhenrique151-design <weslenhenrique151@gmail.com>
2026-03-24 20:20:24 -0300

    primeiro deploy

---
+++ vetores/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim nomeCompleto(2) As String
+        nomeCompleto(0) = "Paulo"
+        nomeCompleto(1) = "Henrique"
+        Console.WriteLine(nomeCompleto(0))
+        Console.WriteLine(nomeCompleto(1))
+    End Sub
+End Module
